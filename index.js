adb52a79e0006db3ab01c885e182c4cbefbe9bb4
elektrowolle <internetbewohner@gmail.com>
2015-05-10 01:43:07 +0200

    xhr

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
